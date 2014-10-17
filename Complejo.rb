4375db11a12db9f5d3c110b6af081b67e8718c63
DEQuijada <dani_ven_tf@hotmail.com>
2014-10-17 13:57:12 +0100

    Operaciones Completadas

--- Complejo.rb
+++ Complejo.rb
@@ -1,27 +1,38 @@
 class Complejo
 	attr_reader :real, :imaginaria
 	attr_writer :real, :imaginaria
+
 	def initialize(real, imaginaria)
 		@real,@imaginaria = real,imaginaria
 	end
 
-	
-
+	#Suma:
 	def +(other)
 		Complejo.new(@real + other.real, @imaginaria + other.imaginaria)
 	end
 
+	#Diferencia:
 	def -(other)
 		Complejo.new(@real - other.real, @imaginaria - other.imaginaria)
 	end
 
-	
-	def div (other)
-		dividendo = ((@real**2) + (other.imaginaria**2))
-		Complejo.new((((@real * other.real) + (@imaginaria * other.imaginaria))/dividendo).round(2), (((@imaginaria * other.real) + (@real * other.imaginaria))/dividendo).round(2))
+	#Multiplicacion:
+	def *(other)
+		Complejo.new(((@real * other.real) - (@imaginaria * other.imaginaria)), ((@real * other.imaginaria) + (@imaginaria * other.real)))
 	end
 	
+	#Division:
+	def /(other)
+		numeradorReal = ((@real * other.real) + (@imaginaria * other.imaginaria))
+		numeradorImaginaria = ((@imaginaria * other.real) - (@real * other.imaginaria))
+		denominador = (other.real**2 + other.imaginaria**2)
+		
+		Complejo.new ((numeradorReal / denominador), (numeradorImaginaria / denominador))
+	end
 	
-	def Pe
+	#Producto por escalar:
+	def  *(y)
+		Complejo.new (@real * y, @imaginaria * y)
+	end
 
 end
